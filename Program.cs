5391314b64c610a08e7b99e131af21ebfb1d0686
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-04 13:17:32 -0300

    Adicionei Comendos

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Etec Adolpho Berezin!");
+﻿Console.Clear();
+Console.WriteLine("Digite seu Nome:");
+Console.WriteLine();
+string NomeUsuario = Console.ReadLine();
+
+Console.WriteLine();
+Console.Write("Ola ");
+Console.Write(NomeUsuario);
+Console.WriteLine(" !, Eu me chamo Andre Nascimento");
+
+Console.WriteLine();
+Console.Write("Aperte uma tecla, ");
+Console.WriteLine("Para ver o nome da minha escola!!");
+Console.ReadKey();
+
+Console.WriteLine();
+Console.WriteLine("Etec Adolpho Berezin !");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine();
+Console.Write("Obrigado, ");
+Console.Write(NomeUsuario);
+Console.WriteLine(" !!");
+Console.ResetColor();
+Console.WriteLine();
+
